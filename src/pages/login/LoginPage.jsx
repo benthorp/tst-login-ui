48665912235e66d9f497438606f1ed1fe39ed2ec
Ben Thorp <ben.thorp@offorhealth.com>
2023-02-15 11:07:12 -0500

    functionality finished for showing and hiding the password matching validation message

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginPage.css';
 import logo from './TST-Logo-Consumer.png';
 
 function LoginPage() {
+  const [verifyPassword, setVerifyPasswordState] = useState('')
+  const [password, setPasswordState] = useState('')
+  const [validationMessage, setValidationMessageState] = useState('')
+
+  const PASSWORDS_MATCH = 'Passwords match';
+  const PASSWORDS_DONT_MATCH = 'Passwords do not match'
+
+  function updateValidationMessage(currentPassword, currentVerifyPassword) {
+    if (!currentVerifyPassword) {
+      setValidationMessageState('')
+    }
+    else if (currentPassword === currentVerifyPassword) {
+      setValidationMessageState(PASSWORDS_MATCH)
+    }
+    else {
+      setValidationMessageState(PASSWORDS_DONT_MATCH)
+    }
+  }
+
+  function handlePasswordChange(event) {
+    const newPassword = event.target.value
+    
+    setPasswordState(event.target.value)
+
+    updateValidationMessage(newPassword, verifyPassword)
+  }
+
+  function handleVerifyPasswordChange(event) {
+    const newVerifyPassword = event.target.value
+
+    setVerifyPasswordState(newVerifyPassword)
+
+    updateValidationMessage(password, newVerifyPassword)
+  }
+
+  function getValidationMessageClass() {
+    if (!validationMessage || validationMessage === PASSWORDS_MATCH) return 'Login-page-password-message-valid'
+
+    return 'Login-page-password-message-invalid'
+  }
+
+  function getInputValidationStatusClass() {
+    if (!validationMessage || validationMessage === PASSWORDS_MATCH) return 'Login-page-input-valid'
+
+    return 'Login-page-input-invalid'
+  }
+
   return (
     <div className='Login-page-container'>
       <header className="Login-page-header">
@@ -23,8 +70,10 @@ function LoginPage() {
           Password
           <input
             type='password'
-            className='Login-page-input'
+            className={'Login-page-input ' + getInputValidationStatusClass()}
             placeholder='Enter Password'
+            onChange={(event) => handlePasswordChange(event)}
+            value={password}
           />
         </label>
       </div>
@@ -33,11 +82,18 @@ function LoginPage() {
           Verify Password
           <input
             type='password'
-            className='Login-page-input'
+            className={'Login-page-input ' + getInputValidationStatusClass()}
             placeholder='Verify Password'
+            onChange={(event) => handleVerifyPasswordChange(event)}
+            value={verifyPassword}
           />
         </label>
       </div>
+      {validationMessage && 
+        <div>
+          <p className={'Login-page-password-message ' + getValidationMessageClass()}>{validationMessage}</p>
+        </div>
+      }
       <button
         type='button'
         className='Login-page-login-button'
--- src/pages/login/LoginPage.test.js
+++ src/pages/login/LoginPage.test.js
@@ -1,21 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import LoginPage from './LoginPage';
+import userEvent from '@testing-library/user-event';
 
 describe('SCENARIO: Login page initial load', () => {
   describe('WHEN: Login page is rendered', () => {
-    it('THEN: username input field is displayed', () => {
+    it('THEN: username input field is displayed with appropriate default value', () => {
       render(<LoginPage />);
 
       const username = screen.getByRole('textbox', { name: 'Username' })
       expect(username.value).toBe('')
     });
-    it('THEN: password input field is displayed', () => {
+    it('THEN: password input field is displayed with appropriate default value', () => {
       render(<LoginPage />);
 
       const password = screen.getByLabelText('Password')
       expect(password.value).toBe('')
     });
-    it('THEN: verify password input field is displayed', () => {
+    it('THEN: verify password input field is displayed with appropriate default value', () => {
       render(<LoginPage />);
 
       const verifyPassword = screen.getByLabelText('Verify Password')
@@ -26,6 +27,132 @@ describe('SCENARIO: Login page initial load', () => {
 
       const loginButton = screen.getByRole('button', { name: /login/i })
       expect(loginButton).toBeInTheDocument()
+    });
+  });
+});
+
+describe('SCENARIO: Verify passwords match', () => {
+  describe('WHEN: both password fields are blank', () => {
+    it('THEN: validation message is not shown', async () => {
+      render(<LoginPage />);
+      const password = screen.getByLabelText('Password')
+      const verifyPassword = screen.getByLabelText('Verify Password')
+
+      await userEvent.clear(password);
+      await userEvent.clear(verifyPassword);
+
+      const validationMessage = screen.queryByText(/passwords match/i)
+      expect(validationMessage).not.toBeInTheDocument()
+    });
+  });
+  describe('WHEN: user types in Verify Password field', () => {
+    describe('AND: passwords match', () => {
+      it('THEN: validation message shows that passwords match', async () => {
+        render(<LoginPage />);
+        const password = screen.getByLabelText('Password')
+        const verifyPassword = screen.getByLabelText('Verify Password')
+
+        await userEvent.type(password, 'MatchingPassword');
+        await userEvent.type(verifyPassword, 'MatchingPassword');
+
+        const validationMessage = screen.getByText(/passwords match/i)
+        expect(validationMessage).toBeInTheDocument()
+      });
+      it('THEN: validation message and fields have appropriate css class', async () => {
+        render(<LoginPage />);
+        const password = screen.getByLabelText('Password')
+        const verifyPassword = screen.getByLabelText('Verify Password')
+
+        await userEvent.type(password, 'MatchingPassword');
+        await userEvent.type(verifyPassword, 'MatchingPassword');
+        const validationMessage = screen.getByText(/passwords match/i)
+
+        expect(validationMessage).toHaveClass('Login-page-password-message Login-page-password-message-valid')
+        expect(password).toHaveClass('Login-page-input Login-page-input-valid')
+        expect(verifyPassword).toHaveClass('Login-page-input Login-page-input-valid')
+      });
+    });
+    describe('AND: passwords do NOT match', () => {
+      it('THEN: validation message shows that passwords to NOT match', async () => {
+        render(<LoginPage />);
+        const password = screen.getByLabelText('Password')
+        const verifyPassword = screen.getByLabelText('Verify Password')
+
+        await userEvent.type(password, 'Password');
+        await userEvent.type(verifyPassword, 'NoMatchy');
+
+        const validationMessage = screen.getByText(/passwords do not match/i)
+        expect(validationMessage).toBeInTheDocument()
+      });
+      it('THEN: validation message and fields have appropriate css class', async () => {
+        render(<LoginPage />);
+        const password = screen.getByLabelText('Password')
+        const verifyPassword = screen.getByLabelText('Verify Password')
+
+        await userEvent.type(password, 'Password');
+        await userEvent.type(verifyPassword, 'NoMatchy');
+        const validationMessage = screen.getByText(/passwords do not match/i)
+
+        expect(validationMessage).toHaveClass('Login-page-password-message Login-page-password-message-invalid')
+        expect(password).toHaveClass('Login-page-input Login-page-input-invalid')
+        expect(verifyPassword).toHaveClass('Login-page-input Login-page-input-invalid')
+      });
+    });
+  });
+  describe('WHEN: user types in Password field', () => {
+    describe('AND: Verify Password field is blank', () => {
+      it('THEN: validation message is not shown', async () => {
+        render(<LoginPage />);
+        const password = screen.getByLabelText('Password')
+        const verifyPassword = screen.getByLabelText('Verify Password')
+
+        await userEvent.type(password, 'MatchingPassword');
+        await userEvent.clear(verifyPassword);
+
+        const validationMessageValid = screen.queryByText(/passwords match/i)
+        const validationMessageInvalid = screen.queryByText(/passwords do not match/i)
+
+        expect(validationMessageValid).not.toBeInTheDocument()
+        expect(validationMessageInvalid).not.toBeInTheDocument()
+      });
+    });
+  });
+  describe('GIVEN: passwords currently match and validation message is displayed', () => {
+    describe('WHEN: Verify Password field is cleared', () => {
+      it('THEN: validation message disappears', async () => {
+        render(<LoginPage />);
+        const password = screen.getByLabelText('Password')
+        const verifyPassword = screen.getByLabelText('Verify Password')
+
+        await userEvent.type(password, 'MatchingPassword');
+        await userEvent.type(verifyPassword, 'MatchingPassword');
+
+        let validationMessage = screen.getByText(/passwords match/i)
+        expect(validationMessage).toBeInTheDocument()
+
+        await userEvent.clear(verifyPassword);
+
+        validationMessage = screen.queryByText(/passwords match/i)
+        expect(validationMessage).not.toBeInTheDocument()
+      });
+    });
+    describe('WHEN: Password field is changed to no longer match Verify Password field', () => {
+      it('THEN: validation message shows that passwords to NOT match', async () => {
+        render(<LoginPage />);
+        const password = screen.getByLabelText('Password')
+        const verifyPassword = screen.getByLabelText('Verify Password')
+
+        await userEvent.type(password, 'MatchingPassword');
+        await userEvent.type(verifyPassword, 'MatchingPassword');
+
+        let validationMessage = screen.getByText(/passwords match/i)
+        expect(validationMessage).toBeInTheDocument()
+
+        await userEvent.type(password, 'NoMoreMatchy');
+
+        validationMessage = screen.getByText(/passwords do not match/i)
+        expect(validationMessage).toBeInTheDocument()
+      });
     });
   });
 });
